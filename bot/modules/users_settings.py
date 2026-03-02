9cec29edd2df22ccb9e70c66f1fb6a297cb269ca
sidbanglatips1 <167536055+sidbanglatips1@users.noreply.github.com>
2026-03-02 14:29:37 +0400

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -13,7 +13,7 @@ from pyrogram.types import CallbackQuery
 from pyrogram.handlers import MessageHandler
 
 from bot.helper.ext_utils.status_utils import get_readable_file_size
-from .. import auth_chats, excluded_extensions, sudo_users, user_dict, logger, bot_loop, PREMIUM_FEATURES, MIN_SAMPLE_IMAGE, MAX_SAMPLE_IMAGE, MIN_SAMPLE_VIDEO, MAX_SAMPLE_VIDEO
+from .. import excluded_extensions, sudo_users, user_dict, logger, bot_loop
 from ..core.config_manager import Config
 from ..core.tg_client import TgClient
 from ..helper.ext_utils.bot_utils import get_size_bytes, new_task, update_user_ldata, has_premium
